c40738a1ab6af86f21b4882866c43bb87bb46d7b
Ann Dominique <estrada.ad@outlook.com>
2026-04-17 12:07:53 +0800

    homepage updated

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function setRole(role, btn) {
     document.querySelectorAll('.type-btn').forEach(b => b.classList.remove('active'));
     btn.classList.add('active');
 }
+
+// ACTIVE FORECAST CARD - index.html
+const today = new Date().getDay();
+
+document.querySelectorAll('.forecast-card').forEach(card => {
+    card.classList.remove('active');
+    
+    if(parseInt(card.getAttribute('data-day')) === today) {
+        card.classList.add('active');
+    }
+});
